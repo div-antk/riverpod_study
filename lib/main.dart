13543ace4d0d9947d1ba77c14c3017a5173f4d6d
div-antk <1984ta@gmail.com>
2023-06-27 23:59:00 +0900

    hooksを使ったカウンターアプリに作り替え

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_study/dogImage.dart'; // 追加
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+// import 'package:riverpod_study/dogImage.dart'; // 追加
 
-final counterProvider = StateProvider<int>((ref) => 0); // 追加
+final counterProvider = StateNotifierProvider((_) => Counter());
+
+class Counter extends StateNotifier<int> {
+  Counter() : super(0);
+  void increment() => state++;
+}
 
 void main() {
   runApp(
-    ProviderScope(
-      child: MaterialApp(
-        title: 'Sample',
-        home: MyApp(),
-      ),
-    ),
+    const ProviderScope(child: CounterApp()),
   );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CounterApp extends HookConsumerWidget {
+  const CounterApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/': (context) => DogImageWidget(),
-      },
-    );
-  }
-}
-
-class CounterWidget extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final counter = ref.watch(counterProvider);
+    final state = ref.watch(counterProvider);
+    final isWhiteBackGround = useState(true);
 
-    void _incrementCounter() {
-      ref.read(counterProvider.notifier).state++;
-    }
-
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Text(
-          'Counter',
-          style: TextStyle(fontSize: 24),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: isWhiteBackGround.value ? Colors.white : Colors.black,
+        appBar: AppBar(
+          title: const Text('カウンターアップ'),
+          leading: IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () => isWhiteBackGround.value = !isWhiteBackGround.value,
+          ),
         ),
-        Text(
-          '$counter',
-          style: const TextStyle(fontSize: 48),
+        body: Center(
+          child: Text(
+            state.toString(),
+            style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: isWhiteBackGround.value ? Colors.black : Colors.white),
+          ),
         ),
-        ElevatedButton(
-          onPressed: _incrementCounter,
-          child: const Text('Increment'),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => ref.read(counterProvider.notifier).increment(),
+          child: const Icon(Icons.add),
         ),
-      ],
+      ),
     );
   }
 }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       initialRoute: '/',
+//       routes: {
+//         '/': (context) => DogImageWidget(),
+//       },
+//     );
+//   }
+// }
+
+// class CounterWidget extends ConsumerWidget {
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final counter = ref.watch(counterProvider);
+
+//     void _incrementCounter() {
+//       ref.read(counterProvider.notifier).state++;
+//     }
+
+//     return Column(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: [
+//         const Text(
+//           'Counter',
+//           style: TextStyle(fontSize: 24),
+//         ),
+//         Text(
+//           '$counter',
+//           style: const TextStyle(fontSize: 48),
+//         ),
+//         ElevatedButton(
+//           onPressed: _incrementCounter,
+//           child: const Text('Increment'),
+//         ),
+//       ],
+//     );
+//   }
+// }
